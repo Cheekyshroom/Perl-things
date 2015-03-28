db3d6d1642c205ba9d0954769d05cce948356bce
cheekyshroom <phernima@ucsc.edu>
2015-03-28 00:57:31 -0700

    Added tile friction and on_step functions

--- Map.pm
+++ Map.pm
@@ -2,6 +2,8 @@ use strict;
 use warnings;
 package Map;
 use Console;
+use Consts;
+use Objects;
 
 sub new_2d_array{
 	my @out;
@@ -16,11 +18,42 @@ sub new_2d_array{
 }
 
 sub new_tile{
-	return {symbol=>' ', on_step=>0, on_update=>0} if $#_ == -1;
-	return {symbol=>$_[0], on_step=>0, on_update=>0} if $#_ == 0;
-	return {symbol=>$_[0], on_step=>$_[1], on_update=>0} if $#_ == 1;
-	return {symbol=>$_[0], on_step=>$_[1], on_update=>$_[2]} if $#_ == 2;
-	return {symbol=>$_[0], on_step=>$_[1], on_update=>$_[2], other_data=>{@_[3..$#_]}};
+	return {symbol=>$_[0], on_step=>$_[1], on_update=>$_[2], friction_coefficient=>$_[3], passable=>$_[4]};
+}
+
+my $tile_types = {
+	cobblestone=>
+		new_tile('.', 0, 0, Consts::FRICTION_DEFAULT, 1),
+	wall=>
+		new_tile('#', 0, 0, 0.0, 0),
+	sand=>
+		new_tile('~', 0, 0, 0.05, 1),
+	mud=>
+		new_tile('%', 0, 0, 0.01, 1),
+	spikes=>
+		new_tile('^',
+			sub{
+				my $object = $_[0];
+				my $map = $_[1];
+				Objects::damage($object, 3);
+				Console::message($object->{"name"}." got damaged for 3 damage");
+			},
+			0,
+			Consts::FRICTION_DEFAULT, 1),
+};
+
+sub new_tile_from_type{
+	return new_tile('.', 0, 0, 0.3) if $_[0] eq "tile";
+	return new_tile('.', 0, 0, 0.3) if $_[0] eq "wall";
+	return new_tile('.', 0, 0, 0.3) if $_[0] eq "sand";
+	return new_tile('.', 0, 0, 0.3) if $_[0] eq "mud";
+	return new_tile('.', 0, 0, 0.3) if $_[0] eq "spikes";
+}
+
+sub new_tile_helper{
+	return new_tile_from_type($_[0]) if $#_ == 0;
+	return new_tile($_[0], $_[1], $_[2], $_[3], $_[4]) if $#_ == 5;
+	return new_tile(' ', 0, 0, 0.3, 1);
 }
 
 sub draw_map{
@@ -40,7 +73,7 @@ sub new_map{
 	my $width = $_[0];
 	my $height = $_[1];
 	my $modifier = $_[2];
-	my $data = new_2d_array($width, $height, sub{return new_tile();});
+	my $data = new_2d_array($width, $height, sub{return new_tile_helper();});
 	return {width=>$width, height=>$height, data=>$modifier->($data, $width, $height)};
 }
 
