ce5cb3fd72580c349aab7cff812f39304f8a86b5
cheekyshroom <phernima@ucsc.edu>
2015-03-28 00:56:58 -0700

    Added friction consts

--- Consts.pm
+++ Consts.pm
@@ -7,5 +7,7 @@ use constant UP => PI+(PI/2);
 use constant DOWN => PI/2;
 use constant LEFT => PI;
 use constant RIGHT => 0.0;
+use constant FRICTION_THRESHOLD => 0.3;
+use constant FRICTION_DEFAULT => 0.3;
 
 1;
