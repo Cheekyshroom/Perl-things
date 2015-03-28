4cb6fdf069f1a8fc5b1a2235e72f0c7310d1989a
cheekyshroom <phernima@ucsc.edu>
2015-03-28 12:21:37 -0700

    No more directional consts

--- Consts.pm
+++ Consts.pm
@@ -3,11 +3,7 @@ use strict;
 use warnings;
 
 use constant PI => 4*atan2(1,1);
-use constant UP => PI+(PI/2);
-use constant DOWN => PI/2;
-use constant LEFT => PI;
-use constant RIGHT => 0.0;
-use constant FRICTION_THRESHOLD => 0.3;
-use constant FRICTION_DEFAULT => 0.3;
+use constant MAP_DISPLAY_WIDTH => 40;
+use constant MAP_DISPLAY_HEIGHT => 20;
 
 1;
