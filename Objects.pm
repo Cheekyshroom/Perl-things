a4b9fe866c22cee1a0cac6a7aa891c99b9d68c42
cheekyshroom <phernima@ucsc.edu>
2015-03-29 12:44:02 -0700

    Objects/players should be able to die now

--- Objects.pm
+++ Objects.pm
@@ -9,7 +9,11 @@ use Map;
 #may as well just store the ref in an object but maybe this copies it?
 sub draw_object{
 	my $oref = $_[0];
-	Console::draw_char($oref->{"y"}, $oref->{"x"}, $oref->{"symbol"});
+	if ($oref->{"alive"}){
+		Console::draw_char($oref->{"y"}, $oref->{"x"}, $oref->{"symbol"});
+	} else {
+		Console::draw_char($oref->{"y"}, $oref->{"x"}, $oref->{"dead_symbol"});
+	}
 }
 
 sub draw_objects{
@@ -19,18 +23,34 @@ sub draw_objects{
 	}
 }
 
-sub new_object($;$;$;$;$;$;$;$){
-	return {x=>$_[0], y=>$_[1], symbol=>$_[2], name=>$_[3], id=>$_[4], health=>$_[5], ap=>$_[6], max_ap=>$_[6], direction=>$_[7]};
+sub new_object($;$;$;$;$;$;$;$;$;$){
+	return {
+		x=>$_[0],
+		y=>$_[1],
+		symbol=>$_[2],	
+		name=>$_[3],
+		id=>$_[4],
+		health=>$_[5],
+		ap=>$_[6], 
+		max_ap=>$_[6], 
+		direction=>$_[7], 
+		alive=>$_[8], 
+		dead_symbol=>$_[9],
+	};
 }
 
 sub move{
 	my $object = $_[0];
-	$object->{"x"}+=$object->{"direction"}->[0];
-	$object->{"y"}+=$object->{"direction"}->[1];
+	if ($object->{"alive"}){
+		$object->{"x"}+=$object->{"direction"}->[0];
+		$object->{"y"}+=$object->{"direction"}->[1];
+	}
 }
 
 sub walk{
-	$_[0]->{"direction"} = $_[1];
+	if ($_[0]->{"alive"}){
+		$_[0]->{"direction"} = $_[1];
+	}
 }
 
 sub damage{
@@ -39,9 +59,11 @@ sub damage{
 
 sub step_object{
 	my $object = $_[0];
-	my $map = $_[1];
-	move($object, $map);
-	Map::activate_on_step($object, $map);
+	if ($object->{"alive"}){
+		my $map = $_[1];
+		move($object, $map);
+		Map::activate_on_step($object, $map);
+	}
 }
 
 1;
